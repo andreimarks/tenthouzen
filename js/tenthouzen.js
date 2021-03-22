60401a3a9eb40d3fb6afeed2ef2991c8258c2c0a
Andrei <hey@ndrei.us>
2021-03-21 18:25:17 -0700

    Index.html

---
+++ js/tenthouzen.js
@@ -0,0 +1,17 @@
+function loadJSON(callback) {
+    var xobj = new XMLHttpRequest();
+    xobj.overrideMimeType("application/json");
+    xobj.open('GET', '../records.json', true);
+    xobj.onreadystatechange = function() {
+        if (xobj.readyState == 4 && xobj.status == "200") {
+            callback(JSON.parse(xobj.responseText));
+        }
+    };
+    xobj.send(null);
+}
+
+console.log("Hello")
+loadJSON(function(json) {
+    console.log(json);
+    document.getElementById("content").textContent = json;
+})
