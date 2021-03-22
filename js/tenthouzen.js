a3f4dbbe9cbfd1c481decfa4e8fbfe84ff302c12
Andrei <hey@ndrei.us>
2021-03-21 18:28:41 -0700

    Fixing path.

--- js/tenthouzen.js
+++ js/tenthouzen.js
@@ -1,7 +1,7 @@
 function loadJSON(callback) {
     var xobj = new XMLHttpRequest();
     xobj.overrideMimeType("application/json");
-    xobj.open('GET', '../records.json', true);
+    xobj.open('GET', '../js/records.json', true);
     xobj.onreadystatechange = function() {
         if (xobj.readyState == 4 && xobj.status == "200") {
             callback(JSON.parse(xobj.responseText));
